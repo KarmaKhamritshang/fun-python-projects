bb9f7bc7985ec476cdde1004fe397e9ae07832b7
Karma Khamrtishang <Karma.23@hotmail.com>
2023-08-05 01:17:07 +0200

    Update quiz-game.py

--- quiz-game.py
+++ quiz-game.py
@@ -7,6 +7,24 @@
 # Author: Karma Khamritshang
 # ------------------------------------------------------------------------
 
+import os
+
+# ------------------------------------------------------------------------
+
+questions = { #dictionary
+    " Who created Python?: ": "A",
+    " What year was Python created?: ": "B",
+    " Python is tributed to which comedy group?: ": "C",
+    " Is the Earth round?: ": "A"
+}
+
+options = [[" A. Guido van Rossum", " B. Elon Musk", " C. Bill Gates", " D. Mark Zuckerberg"], #2d list - tuples
+          [" A. 1989", " B. 1991", " C. 2000", " D. 2016"],
+          [" A. Lonely Island", " B. Smosh", " C. Monty Python", " D. SNL"],
+          [" A. True", " B. False", " C. sometimes", " D. What's Earth?"]]
+
+# ------------------------------------------------------------------------
+
 def new_game():
     
     guesses = []
@@ -14,24 +32,33 @@ def new_game():
     question_num = 1
     
     for key in questions:
-        print("------------------------------------------")
+        print("")
         print(key)
         print("------------------------------------------")
         for i in options[question_num-1]:
             print(i)
         
-        
-        guess = input("Enter (A, B, C or D): ").upper()
         print("------------------------------------------")
+        guess = input("Enter (A, B, C or D): ").upper()
+        
         guesses.append(guess)
         correct_guesses += check_answer(questions.get(key),guess)
-        
         question_num += 1
+        clear_screen()
         
     display_score(correct_guesses, guesses)
     
-        
+
+# ------------------------------------------------------------------------   
+    
+def clear_screen():
+    # Check if the OS is Windows
+    if os.name == 'nt':
+        os.system('cls')
+    else:  # Assume Unix-based system
+        os.system('clear')
 # ------------------------------------------------------------------------
+
 def check_answer(answer, guess):
     if answer == guess:
         print("CORRECT!")
@@ -39,45 +66,38 @@ def check_answer(answer, guess):
     else:
         print("WRONG!")
         return 0
+    
 # ------------------------------------------------------------------------
+
 def display_score(correct_guesses, guesses):
+    print("")
+    print(" RESULTS")
     print("------------------------------------------")
-    print("RESULTS")
-    print("------------------------------------------")
-    print("Answers: ", end="")
+    print(" Solution: ", end="")
     for i in questions:
         print(questions.get(i), end=" ")
     print()
-    print("Guesses: ", end="")
+    print(" My Guesses: ", end="")
     for i in guesses:
         print(i, end=" ")
-    print()
-    
+    print("")
+    print("------------------------------------------")
     score = int((correct_guesses/len(questions)*100))
     print("Your score is: "+str(score)+"%")
+    print("------------------------------------------")
     
 # ------------------------------------------------------------------------
+
 def play_again():
     response = input("Do you want to play again? (yes or no): ").upper()
     if response == "YES":
         return True
     else:
         return False
+    
 # ------------------------------------------------------------------------
 
-
-questions = { #dictionary
-    "Who created Python?: ": "A",
-    "What year was Python created?: ": "B",
-    "Python is tributed to which comedy group?: ": "C",
-    "Is the Earth round?: ": "A"
-}
-
-options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerberg"], #2d list - tuples
-          ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
-          ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
-          ["A. True", "B. False", "C. sometimes", "D. What's Earth?"]]
-
+clear_screen()
 new_game()
 
 while play_again():
